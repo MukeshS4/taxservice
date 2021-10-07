529796996476327fd0e7f685377513cd04b8a7a8
suryakantsurve@gmail.com <john@gmail.com>
2021-10-07 16:26:44 +0530

    A

--- src/main/java/com/citius/taxservice/controller/TaxController.java
+++ src/main/java/com/citius/taxservice/controller/TaxController.java
@@ -2,8 +2,11 @@ package com.citius.taxservice.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.citius.taxservice.model.Tax;
@@ -11,12 +14,22 @@ import com.citius.taxservice.service.TaxService;
 
 @RestController
 public class TaxController {
+	private static Logger log = LoggerFactory.getLogger(TaxController.class);
 
 	@Autowired
 	private TaxService taxService;
+
 	@GetMapping("/taxes")
-	public List<Tax> getTaxes(){
+	public List<Tax> getTaxes() {
 		return taxService.getAll();
-		
+
 	}
+
+	@GetMapping("/taxes/{id}")
+	public Tax getTax(@PathVariable int id) {
+		log.info("Tracing Taxes");
+
+		return taxService.getTax(id);
+	}
+
 }
--- src/main/java/com/citius/taxservice/service/TaxService.java
+++ src/main/java/com/citius/taxservice/service/TaxService.java
@@ -1,4 +1,5 @@
 package com.citius.taxservice.service;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -6,14 +7,20 @@ import com.citius.taxservice.model.Tax;
 
 @Service
 public class TaxService {
-	
+
 	private List<Tax> taxes = new ArrayList<Tax>();
+
 	public TaxService() {
 		taxes.add(new Tax(1, 1, 0.5));
 		taxes.add(new Tax(2, 2, 1.0));
 	}
-	public List<Tax> getAll(){
+
+	public List<Tax> getAll() {
 		return taxes;
+	}
+
+	public Tax getTax(int id) {
+		return taxes.stream().filter((c) -> c.getId() == id).findFirst().get();
 	}
 
 }
